177ca85bc3ff19e1dc4781926e40665ecbc0e034
upsharma8 <55279749+upsharma8@users.noreply.github.com>
2020-11-06 22:00:35 +0530

    Add files via upload

---
+++ Final/main.py
@@ -0,0 +1,58 @@
+
+import os
+import getpass
+import docker
+import hadoop
+import webserver
+
+print("\t\t\tWelcome to Automation of Different Technologies Menu")
+
+print("\t\t\t--------------------------------------")
+
+passwd = getpass.getpass("Enter your password: ")
+
+if passwd != "toor":
+   print("password is incorrect....")
+   exit()
+
+print("\t\t\t\t\tMenu")
+print("\t\t\t\t\t----")
+
+
+
+while(True):
+    print("""
+ Press  1 : For Docker
+ Press  2 : For Hadoop
+ Press  3 : For aws 
+ Press  4 : For lvm 
+ Press  5 : For Webserver Automation
+ 
+""")
+    
+    ch=int(input("Enter Your Choice"))
+
+    if ch == 1:
+        docker.docker() 
+
+    elif ch == 2:
+        hadoop.hadoop()
+
+    elif ch == 3:
+        print("Aws not available")
+
+
+    elif ch == 4:
+              
+        print("LVM Not Avilable")
+
+
+    elif ch == 5:
+        webserver.webserver()            
+        
+
+    elif ch == 6:
+        exit()
+        
+    else:
+        print("You Entered Wrong Choice ...")
