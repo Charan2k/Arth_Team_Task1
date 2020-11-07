799ec162ce2614583c52da0e0d141112c93c5d3f
Charan Vaddepally <charan316.cv@gmail.com>
2020-11-07 13:55:16 +0530

    Update hadoop.py

--- Final/hadoop.py
+++ Final/hadoop.py
@@ -1,45 +1,50 @@
+# Module for Hadoop Automation 
 import os
+
 def hadoop():
-    while(True):
-                       
-        print("Press 1: To Start your Name Node")
-        print(i"Press 2: To check hadoop services are running")
-        print("Press 3: TO stop your name node")
-        print("Press 4: To start your data node")
-        print("Press 5: To check hdfs file system") 
-        print("Press 6 : To stop the datanode")
-        print("Press 7: To check the report")
-        print("Press 8: To Exit Back to Main Menu")
-        i=int(input("Enter your choice"))
-        if i==1:
-            os.system("hadoop-daemon.sh start namenode")
-        elif i==2:
-            print("Press 1 for checking namenode service")
-            print("Press 2 for checking datanode service")
-            k=int(input("Enter the choice"))
-            if k==1:
-                os.system("jps")
-            elif k==2:
-                j=input("Enter the ip of data node")
-                os.system("ssh {} jps".format(j))	
-        elif i==3:
-            os.system("hadoop-daemon.sh stop namenode")
-        elif i==4:
-            b=int(input("Enter the no of data nodes you want to start"))
-            for i in range(b):
-                j=input("Enter the Ip address of node {}".format(i))
-                os.system("ssh {} hadoop-daemon.sh start datanode".format(j))
-        elif i==5:
-            os.system("hadoop fs -ls /")		
-        elif i==6:
-            i=input("Enter the Ip address of the data node to stop")
-            os.system("ssh {} hadoop-daemon.sh stop datanode".format(i))
-        elif i==7:
-            os.system("hadoop dfsadmin -report")
-        elif i==8:
-            break
-        else:
-            print("Wrong choice")
-        
-            
-	 
+	while(True):
+		print("""------------------MENU------------------
+		Press 1 : To Start your Name Node
+		Press 2 : To check hadoop services are running
+		Press 3 : TO stop your name node
+		Press 4 : To start your data node
+		Press 5 : To check hdfs file system 
+		Press 6 : To stop the datanode
+		Press 7 : To check the report
+		""")
+
+		choice=int(input("Enter your choice"))
+		if choice == 1:
+			os.system("hadoop-daemon.sh start namenode")
+
+		elif choice == 2:
+			print("Press 1 for checking namenode service")
+			print("Press 2 for checking datanode service")
+			k=int(input("Enter the choice"))
+			if k == 1:
+				os.system("jps")
+			elif k == 2:
+				j = input("Enter the ip of data node")
+				os.system("ssh {} jps".format(j))
+
+		elif choice == 3:
+			os.system("hadoop-daemon.sh stop namenode")
+
+		elif choice == 4:
+			b = int(input("Enter the no of data nodes you want to start"))
+			for i in range(b):
+				j = input("Enter the Ip address of node {}".format(i))
+				os.system("ssh {} hadoop-daemon.sh start datanode".format(j))
+
+		elif choice == 5:
+			os.system("hadoop fs -ls /")
+
+		elif choice == 6:
+			ip = input("Enter the Ip address of the data node to stop: ")
+			os.system("ssh {} hadoop-daemon.sh stop datanode".format(ip))
+
+		elif choice == 7:
+			os.system("hadoop dfsadmin -report")
+
+		else:
+			print("Wrong choice")
